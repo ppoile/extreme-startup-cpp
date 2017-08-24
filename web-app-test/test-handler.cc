c0498716d081d2706725cae956410d04149eeae9
Andreas Meier <andreas.meier@bbv.ch>
2017-08-24 16:27:29 +0200

    add tests

--- web-app-test/test-handler.cc
+++ web-app-test/test-handler.cc
@@ -12,4 +12,18 @@ TEST(A_Handler, should_return_its_name_when_asked_for) {
 
   ASSERT_THAT(testee.handle(query), Eq("skm_team"));
 }
+
+TEST(A_Handler, should_return_power_calculations_when_asked_for) {
+    std::string const query = "what is 1 to the power of 16";
+
+    ASSERT_THAT(testee.handle(query), Eq(""));
+}
+
+TEST(A_Handler, should_return_sum_when_asked_for) {
+    std::string const query = "what is 13 plus 2 plus 4";
+
+    ASSERT_THAT(testee.handle(query), Eq("19"));
+}
+
+
 }
